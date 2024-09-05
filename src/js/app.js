e40dfe067a2470ee6015055e9e869dc00dbbf459
Yelyzaveta Tretiak <liza.tretjak@gmail.com>
2024-09-05 22:14:01 +0200

    added bluring of background when there's a popup

--- src/js/app.js
+++ src/js/app.js
@@ -8,26 +8,28 @@ document.addEventListener('DOMContentLoaded', function () {
         openPopup()
     }))
 
-    const popupOverlay = document.getElementById('popupOverlay');
+    const popupOverlay = document.getElementById('popupOverlay')
 
-    const popup = document.getElementById('popup');
+    const popup = document.getElementById('popup')
 
-    const closePopup = document.getElementById('closePopup');
-    
+    const closePopup = document.getElementById('closePopup')
+    const content = document.getElementById('page-content')
+
     function openPopup() {
-        popupOverlay.style.display = 'inline-block';
-
+        popupOverlay.style.display = 'inline-block'
+        content.classList.add('blurred')
     }
 
     function closePopupFunc() {
-        popupOverlay.style.display = 'none';
+        popupOverlay.style.display = 'none'
+        content.classList.remove('blurred')
     }
 
     openPopup();
     closePopup.addEventListener('click', closePopupFunc);
     popupOverlay.addEventListener('click', function (event) {
         if (event.target === popupOverlay) {
-            closePopupFunc();
+            closePopupFunc()
         }
     });
 });
