3c361212b4f0efc5eba5570ad62944dbff5f21e9
Yelyzaveta Tretiak <liza.tretjak@gmail.com>
2024-09-05 16:48:42 +0200

    started process on pop up

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,35 @@
-const testModules = require('./test-module');
+/* const testModules = require('./test-module');
 require('../css/app.css');
 
 /** ******** Your code here! *********** */
+document.addEventListener('DOMContentLoaded', function () {
+    const buttonsAddTeacher = document.querySelectorAll('.button-add-teacher')
+    buttonsAddTeacher.forEach(el => el.addEventListener('click', event => {
+        openPopup()
+    }))
+
+    const popupOverlay = document.getElementById('popupOverlay');
+
+    const popup = document.getElementById('popup');
+
+    const closePopup = document.getElementById('closePopup');
+    
+    function openPopup() {
+        popupOverlay.style.display = 'inline-block';
+
+    }
+
+    function closePopupFunc() {
+        popupOverlay.style.display = 'none';
+    }
+
+    openPopup();
+    closePopup.addEventListener('click', closePopupFunc);
+    popupOverlay.addEventListener('click', function (event) {
+        if (event.target === popupOverlay) {
+            closePopupFunc();
+        }
+    });
+});
 
 console.log(testModules.hello);
