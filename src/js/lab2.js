37b67c0399c3c13ba32af013df98c8893d7547c9
Yelyzaveta Tretiak <liza.tretjak@gmail.com>
2024-09-13 20:48:57 +0200

    task #4 of lab2 is done

--- src/js/lab2.js
+++ src/js/lab2.js
@@ -52,7 +52,92 @@ function getFormattedUsers(randomUserMock, additionalUsers) {
     return joinedArrays
 }
 
-console.log(getFormattedUsers(randomUserMock, additionalUsers)[0])
+
+// task 2
+// TODO: finish validation - by gender and phone
+function validateUser(user) {
+    return isStringAndStartsWithCapitalLetter(user.full_name)
+   /* && isStringAndStartsWithCapitalLetter(user.gender)
+    && isStringAndStartsWithCapitalLetter(user.note)
+    && isStringAndStartsWithCapitalLetter(user.state)
+    && isStringAndStartsWithCapitalLetter(user.city)
+    && isStringAndStartsWithCapitalLetter(user.country)*/
+    && isANumber(user.age)
+    && isEmail(user.email)
+}
+
+function isStringAndStartsWithCapitalLetter(value) {
+    return typeof value === 'string' && value.charAt(0) === value.charAt(0).toUpperCase()
+}
+
+function isANumber(value) {
+    return typeof value === 'number'
+}
+
+function isEmail(value) {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    return regex.test(value)
+}
+
+
+// task 3
+//filter the array of users by specified values
+function filterUsers(arrayOfUsers, chosenCountry, chosenAge, chosenGender, chosenFavorite) {
+    return arrayOfUsers.filter(user => {
+        return (
+            (chosenCountry === undefined || chosenCountry === user.country)
+            && (chosenAge === undefined || chosenAge === user.age)
+            && (chosenGender === undefined || chosenGender === user.gender)
+            && (chosenFavorite === undefined || chosenFavorite === user.favorite)
+        )
+        })
+}
+
+
+// task 4
+function sortUsers(arrayOfUsers, sortBy, order='asc') {
+    if(sortBy === 'full_name') {
+        if(order === 'asc')
+            return arrayOfUsers.sort((a, b) => a.full_name.localeCompare(b.full_name))
+        else return arrayOfUsers.sort((a, b) => b.full_name.localeCompare(a.full_name))
+    }
+
+    else if (sortBy === 'age') {
+        if(order === 'asc')
+            return arrayOfUsers.sort((a, b) => a.age - b.age)
+        else return arrayOfUsers.sort((a, b) => b.age - a.age)
+    }
+
+    else if (sortBy === 'b_date') {
+        if (order === 'asc')
+            return arrayOfUsers.sort((a, b) => new Date(a.b_date) - new Date(b.b_date))
+        else return arrayOfUsers.sort((a, b) => new Date(b.b_date) - new Date(a.b_date))
+    }
+
+    else if (sortBy === 'country') {
+        if(order === 'asc')
+            return arrayOfUsers.sort((a, b) => a.country.localeCompare(b.country))
+        else return arrayOfUsers.sort((a, b) => b.country.localeCompare(a.country))
+    }
+}
+
+
+const formatted = getFormattedUsers(randomUserMock, additionalUsers)
+
+console.log(formatted)
 console.log(`size of randomUsers = ${randomUserMock.length}`)
 console.log(`size of additionalUsers = ${additionalUsers.length}`)
-console.log(`size of formatted array = ${getFormattedUsers(randomUserMock, additionalUsers).length}`)
+console.log(`size of formatted array = ${formatted.length}`)
+
+console.log(`${isStringAndStartsWithCapitalLetter('9')}`)
+
+console.log(formatted[0])
+console.log(`validating Norbert = ${validateUser(formatted[0])}`)
+
+let filtered = filterUsers(formatted, 'Germany', undefined, 'female')
+console.log('FILTERED')
+console.log(filtered)
+
+console.log('SORT BY AGE ASC')
+let sorted = sortUsers(formatted, 'b_date', 'asc')
+console.log(sorted)
